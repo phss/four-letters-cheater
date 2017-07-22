b7fb04ed99eaf299921900c9054511c8946bc198
Paulo Schneider <paulo.schneider@gmail.com>
2017-07-22 11:55:55 +0100

    Save words in a dictionary

--- experiments/word-picking.py
+++ experiments/word-picking.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 
 # Given four letters, come up with an acceptable random word
+from collections import defaultdict
 
 class Dictionary(object):
   def __init__(self, words_file_path):
+    self.dict = defaultdict(list)
+
     with open(words_file_path) as file:
       for line in file:
-        print line
+        word = line.strip()
+        self.dict[self._str_key(word)].append(word)
+        
+  def _str_key(self, s):
+    return ''.join(sorted(s))
+
 
 def main():
   dictionary = Dictionary('/usr/share/dict/words')
