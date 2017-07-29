6803929bb83bc51f8b58c54a1f39b8021516c5cb
Paulo Schneider <paulo.schneider@gmail.com>
2017-07-29 23:10:14 +0100

    Extract individual letter images

--- experiments/extract-letters-from-screen.py
+++ experiments/extract-letters-from-screen.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 
 # From an Android screen grab, extract the four letter images
+from skimage import data, io, filters
 
 def extract(file_path):
-  None
+  image = io.imread(file_path)
+  width, height = 237, 255
+  image_dims = [(420, 937), (185, 1167), (655, 1167), (420, 1400)]
+
+  for i, (x, y) in enumerate(image_dims):
+    sub = image[y:(y+height), x:(x+width)]
+    io.imsave("experiments/data/extracted_%d.png" % i, sub)
 
 if __name__ == '__main__':
-  extract('data/screen.png')
+  extract('experiments/data/screen.png')
