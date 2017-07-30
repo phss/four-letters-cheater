43aa3a9f904f67c288738634928d82fcf19d8dc7
Paulo Schneider <paulo.schneider@gmail.com>
2017-07-30 19:48:41 +0100

    Extract colors to a plain black or white

--- experiments/extract-letters-from-screen.py
+++ experiments/extract-letters-from-screen.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 
 # From an Android screen grab, extract the four letter images
-from skimage import data, io, filters
+from skimage import io, color
+from collections import Counter
+
+def explode_color(image):
+  flattened_image = image.reshape(-1)
+  for i, p in enumerate(flattened_image):
+    new_p = 1
+    if p > 0.9:
+      new_p = 0
+    flattened_image[i] = new_p
 
 def extract(file_path):
   image = io.imread(file_path)
@@ -10,6 +19,8 @@ def extract(file_path):
 
   for i, (x, y) in enumerate(image_dims):
     sub = image[y:(y+height), x:(x+width)]
+    sub = color.rgb2gray(sub)
+    explode_color(sub)
     io.imsave("experiments/data/extracted_%d.png" % i, sub)
 
 if __name__ == '__main__':
